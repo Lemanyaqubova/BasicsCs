12e0fdcd9a03788e9ce023d88e31fc3c8fe623f4
lyamy <lamanny@code.edu.az>
2023-08-27 15:50:30 +0400

    if and switch case

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿int scrore = 86;
+
+if (scrore >= 88 && scrore <= 100)
+{
+    Console.WriteLine("Success-AA");
+}
+else if (scrore >= 81 && scrore < 88)
+{
+    Console.WriteLine("Success-BA");
+}
+else if (scrore >= 74 && scrore < 81)
+{
+    Console.WriteLine("Success-BB");
+}
+else if (scrore >= 67 && scrore < 74)
+{
+    Console.WriteLine("Success-CB");
+}
+else if (scrore >= 60 && scrore < 67)
+{
+    Console.WriteLine("Success-CC");
+}
+else if (scrore >= 0 && scrore < 60)
+{
+    Console.WriteLine("Fail - FF");
+}
+else
+{
+    Console.WriteLine("Invalid score");
+}
+
+
+
+string command = "stop";
+switch (command)
+{
+    case "start":
+        Console.WriteLine("Game is starting");
+        break;
+    case "stop":
+        Console.WriteLine("Game stopped");
+        break;
+    case "wait":
+        Console.WriteLine("Game paused");
+        break;
+    default:
+        Console.WriteLine("Command not found");
+        break;
+}
